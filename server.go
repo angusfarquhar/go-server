bf21d9b3e8a7a78cf69fb6ca6c30f7f554a447e5
Angus Farquhar <mr.angus.farquhar@gmail.com>
2021-08-10 18:18:42 +1000

    add json encoder

--- server.go
+++ server.go
@@ -3,13 +3,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	_, err := fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	names := r.URL.Query()["name"]
+	var name string
+	if len(names) == 1 {
+		name = names[0]
+	}
+	m := map[string]string{"name": name}
+	enc := json.NewEncoder(w)
+	enc.Encode(m)
+
+	_, err := fmt.Fprintf(w, "Hi there, I love %s!", m["name"])
 
 	if err != nil {
 		log.Fatal(err)
